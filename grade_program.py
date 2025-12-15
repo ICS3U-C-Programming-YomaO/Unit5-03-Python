8cad4085db937c7164c0e94e904c6c9358f4d59c
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-12-15 01:11:50 +0000

    markpercent

---
+++ grade_program.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Created By: Yoma Ozoh
+# Date: November, 2025
+# This program changes grade level to percentage
+def calc_mark(level):
+
+    # set grades level to percentages
+    grade = -1
+    if level == "4++":
+        grade == 95
+    elif level == "4+":
+        grade = 90
+    elif level == "4":
+        grade = 85
+    elif level == "4-":
+        grade = 80
+    elif level == "3+":
+        grade = 79
+    elif level == "3":
+        grade = 75
+    elif level == "3-":
+        grade = 70
+    elif level == "2+":
+        grade = 69
+    elif level == "2":
+        grade = 65
+    elif level == "2-":
+        grade = 60
+    elif level == "1+":
+        grade = 58
+    elif level == "1":
+        grade = 55
+    elif level == "1-":
+        grade = 50
+
+    # invalid input stays as -1
+    return grade
+
+
+def main():
+
+    level = input("Enter you grade level (1-, 2, 3+, 4++): ")
+
+    mark = calc_mark(level)
+
+    if mark == -1:
+        print("Invalid level entered.")
+
+    else:
+        print("The middle percentage for ", level, " is ", str(mark), "%.")
+
+
+if __name__ == "__main__":
+
+    main()
